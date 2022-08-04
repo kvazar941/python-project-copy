4c0a2aa3ad1827d48129f7dfc02f00d899a8e2ce
Sergey <kvazar941@gmail.com>
2022-08-05 00:16:46 +0300

    settings path to css

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "task_manager", "assets")
+STATIC_ROOT = os.path.join(BASE_DIR, "task_manager", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
