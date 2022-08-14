d9f73d8bde9e59ddafcc0e7e70d817e201a45f82
Sergey <kvazar941@gmail.com>
2022-08-14 22:51:13 +0300

    add templates to add, delete, update users

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
 from task_manager import views
-from task_manager.views import task_manager_detail2
+from task_manager.views import users, users_create, users_update, users_delete, login, logout
 from django.conf.urls.i18n import i18n_patterns
 
 
 appname = 'task_manager'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    #path('', include('templates.index', namespace='task_manager')),
-    #path('', include('task_manager.urls', namespace='task_manager')),
-    #path('<slug:slug>', task_manager_detail, name='task_manager_detail'),
-    path('2/', views.task_manager_detail2),
+    path('', views.home),
+    path('users/', views.users),
+    path('users/create/', views.users_create),
+    path('users/<int:pk>/update/', views.users_update),
+    path('users/<int:pk>/delete/', views.users_delete),
+    path('login/', views.login),
+    path('logout/', views.logout),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -2,23 +2,75 @@ from django.shortcuts import get_object_or_404, render
 from .models import my_class
 
 
-def index(request):
-    return render(request, 'index.html', context={
+def home(request):
+    return render(request, 'home.html', context={
         'who': 'World',
     })
 
 
-def task_manager_detail(request, slug):
-    instance = get_object_or_404(my_class, slug=slug)
-    context = {
-        'instanse': instance,
-    }
-    return render(request, 'task_manager/templates/task_manager_detail.html', context)
-
-
-def task_manager_detail2(request):
+def users(request):
     #instance = get_object_or_404(my_class, slug='2')
     context = {
-        'who': 'task manager',
+        'who': 'users',
     }
-    return render(request, 'task_manager_detail.html', context)
+    return render(request, 'users.html', context)
+
+
+def users_create(request):
+    if request.method == 'GET':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'users_create.html', context)
+    elif request.method == 'POST':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'users_create.html', context)
+
+
+def users_update(request):
+    if request.method == 'GET':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'users_update.html', context)
+    elif request.method == 'POST':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'users_update.html', context)
+
+
+def users_delete(request):
+    if request.method == 'GET':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'users_delete.html', context)
+    elif request.method == 'POST':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'users_delete.html', context)
+
+
+def login(request):
+    if request.method == 'GET':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'login.html', context)
+    elif request.method == 'POST':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'login.html', context)
+
+
+def logout(request):
+    if request.method == 'POST':
+        context = {
+            'who': request.method,
+        }
+        return render(request, 'logout.html', context)
