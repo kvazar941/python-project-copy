da31dcda3b7e50454ebf8bec72860ec799bd8ae7
Sergey <kvazar941@gmail.com>
2022-09-07 21:55:10 +0300

    refactoring code

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv()
-env_path = Path('..') / '.env'
+env_path = BASE_DIR/'.env'
 load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
