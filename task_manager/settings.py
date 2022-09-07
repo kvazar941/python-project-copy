71d6344a509d9502f0818b9ed5a968e5309b50b8
Sergey <kvazar941@gmail.com>
2022-09-07 22:42:56 +0300

    change settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     },
 }
+
 POSTGRES_DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': os.environ.get('POSTGRES_NAME'),
