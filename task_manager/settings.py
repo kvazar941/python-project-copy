8541a12c6025a9ba20c53ea50f47e350fa951371
Sergey <kvazar941@gmail.com>
2022-09-07 21:57:07 +0300

    refactoring code

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -3,12 +3,13 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 load_dotenv()
 env_path = BASE_DIR/'.env'
 load_dotenv(dotenv_path=env_path)
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
