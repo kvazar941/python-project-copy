d54dd09085f145a3a9779b4ae98043906b08bfe2
Sergey <kvazar941@gmail.com>
2022-09-08 15:31:36 +0300

    setting secret to github

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -15,7 +15,7 @@ if exists(dotenv_path):
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "not_empty_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
