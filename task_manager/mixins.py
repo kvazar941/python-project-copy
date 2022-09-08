326eda79e6e24d8234b1439fabb199cc8adb1d5a
Sergey <kvazar941@gmail.com>
2022-09-08 22:45:11 +0300

    changed mixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -23,7 +23,18 @@ class CheckDeleteMixin(AccessMixin):
     redirect_delete_url = ''
 
     def form_valid(self, form):
-        self.object.delete()
+        try:
+            self.object.delete()
+        except ProtectedError:
+            messages.error(
+                self.request,
+                gettext_lazy(self.error_delete_message),
+            )
+        else:
+            messages.success(
+                self.request,
+                gettext_lazy(self.success_delete_message),
+            )
         return HttpResponseRedirect(reverse_lazy(self.redirect_delete_url))
 
 
