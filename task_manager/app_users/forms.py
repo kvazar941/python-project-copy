a08f2e3f96a6ec48ff8262e4a3ee4be113d516fa
Sergey <kvazar941@gmail.com>
2022-09-12 23:23:40 +0300

    changed app_users/forms.py

--- task_manager/app_users/forms.py
+++ task_manager/app_users/forms.py
@@ -1,9 +1,22 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from task_manager.app_users.models import ApplicationUsers
 
+MAX_LENGTH = 100
+
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=MAX_LENGTH,
+        label=('Name'),
+        required=True,
+    )
+    last_name = forms.CharField(
+        max_length=MAX_LENGTH,
+        label=('LastName'),
+        required=True,
+    )
 
     class Meta:
         model = ApplicationUsers
